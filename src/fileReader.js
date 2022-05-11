5dc8ec2e900ed0f4a4978e0dcac9fd21c7ae8c5c
Gerardo Camacho <gdotcamacho@gmail.com>
2022-05-11 11:53:34 -0600

    Add readJorunal & readIndexFile functions

---
+++ src/fileReader.js
@@ -0,0 +1,30 @@
+import fs from "fs";
+import path from "path";
+
+export function readJournalFile(location) {
+  try {
+    const rawData = fs.readFileSync(location, "utf-8");
+
+    let linesOfContent = rawData.split("\n");
+
+    if (linesOfContent[0].startsWith("!include")) {
+      linesOfContent = readIndexFile(linesOfContent, location);
+    }
+
+    return linesOfContent;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+
+}
+export function readIndexFile(index, indexLocation) {
+  const linesOfContent = [];
+  index.forEach((line) => {
+    if (!line) return;
+    const fileName = line.split(" ")[1];
+    const baseLocation = path.dirname(indexLocation);
+    const fileLocation = path.join(baseLocation, fileName);
+    linesOfContent.push(...readJournalFile(fileLocation));
+  });
+  return linesOfContent;
+}
